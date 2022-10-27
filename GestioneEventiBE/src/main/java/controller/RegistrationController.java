0d5d50db94f1f11a6801ce4e3fb8d114207d42d7
eledaveri <elena.daveri00@gmail.com>
2022-10-27 16:27:25 +0200

    Creazione registration controller

---
+++ GestioneEventiBE/src/main/java/controller/RegistrationController.java
@@ -0,0 +1,82 @@
+package controller;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Servlet implementation class RegistrationController
+ */
+public class RegistrationController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistrationController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void init()
+    {
+    	System.out.println("mi connetto al db");
+    	connection = connectToDb();
+    	System.out.println("connessione avvenuta con successo");
+    }
+    
+    private Connection connectToDb(){
+		try {
+			// The following lines takes the parameters used to log into the database
+			ServletContext context = getServletContext();
+			String driver = context.getInitParameter("dbDriver");
+			String url = context.getInitParameter("dbUrl");
+			String user = context.getInitParameter("dbUser");
+			String password = context.getInitParameter("dbPassword");
+			// This initializes the driver used to interact with the database
+			Class.forName(driver);
+			// This returns the connection
+			return DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			return null;
+		}
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		connectToDb();
+		StringBuilder buffer = new StringBuilder();
+	    BufferedReader reader = request.getReader();
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+	        buffer.append(line);
+	        buffer.append(System.lineSeparator());
+	    }
+	    String data = buffer.toString();
+	    System.out.println("i parametri sono:" +data);
+		//doGet(request, response);
+	}
+
+}
