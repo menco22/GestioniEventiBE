01c66e2baacca158c428960684f20fa3b6b503bb
menco22 <mencucci.marco@gmail.com>
2022-10-27 16:42:35 +0200

    Creazione RegistrationBean

---
+++ GestioneEventiBE/src/main/java/beans/RegistrationBean.java
@@ -0,0 +1,40 @@
+package beans;
+
+public class RegistrationBean {
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getIdRole() {
+		return idRole;
+	}
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+	private String name;
+	private String surname;
+	private String email;
+	private String password;
+	private int idRole;
+
+}
