2cbff52342f9dc83618ec125bd1cdff61ccc707e
eledaveri <elena.daveri00@gmail.com>
2022-12-02 20:07:51 +0100

    user profile

---
+++ GestioneEventiBE/src/main/java/controller/UserProfileController.java
@@ -0,0 +1,90 @@
+package controller;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.google.gson.Gson;
+
+import beans.UserBean;
+import dao.UserDao;
+
+/**
+ * Servlet implementation class UserProfileController
+ */
+public class UserProfileController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection; 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserProfileController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void init()
+    {
+    	System.out.println("mi connetto al db");
+    	connection = connectToDb();
+    	System.out.println("connessione avvenuta con successo");
+    }
+    
+    private Connection connectToDb(){
+		try {
+			// The following lines takes the parameters used to log into the database
+			ServletContext context = getServletContext();
+			String driver = context.getInitParameter("dbDriver");
+			String url = context.getInitParameter("dbUrl");
+			String user = context.getInitParameter("dbUser");
+			String password = context.getInitParameter("dbPassword");
+			// This initializes the driver used to interact with the database
+			Class.forName(driver);
+			// This returns the connection
+			return DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			return null;
+		}
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		AuthenticationController auth = new AuthenticationController(request);
+		String profileResponse = "";
+		UserDao userDao = new UserDao (this.connection);
+		if(auth.checkToken(request)==true) {
+			try {
+				UserBean user = userDao.getUserById(auth.getIdUser(request));
+				profileResponse = new Gson().toJson(user);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		response.getWriter().append(profileResponse);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
