55242c9fe78c512f25fa2ced08344a7ac26b2a9e
eledaveri <elena.daveri00@gmail.com>
2023-01-19 17:18:37 +0100

    errata corrige

--- GestioneEventiBE/src/main/java/controller/EventController.java
+++ GestioneEventiBE/src/main/java/controller/EventController.java
@@ -126,7 +126,7 @@ public class EventController extends HttpServlet {
 						BookingDao bookingDao = new BookingDao(this.connection);
 						ArrayList<BookingBean> bookings = bookingDao.getBookingByUser(orderBy, orderDirection, auth.getIdUser(request));
 						ArrayList <EventBean> eventList = eventDao.getEvents(orderBy, orderDirection);
-						for(int i = 0; i < bookings.size(); i++) {
+						for(int i = 0; i < eventList.size(); i++) {
 							if(bookings.get(i).getEvent().getIdEvent() == eventList.get(i).getIdEvent()) {
 								eventList.remove(i);
 							}	
